d596d59900075dabfa80a8e47ef1e566959967be
Bojoly Marc <mbojoly@octo.com>
2017-02-01 19:58:14 +0100

    Unit test ok on SlideExtractor

--- src/main/java/com/octo/mbo/SlideExtractor.java
+++ src/main/java/com/octo/mbo/SlideExtractor.java
@@ -35,65 +35,50 @@ class SlideExtractor {
 
         Part partSrc = hme.getValue();
 
-        if (hme.getValue() instanceof SlidePart) {
+        if (partSrc != null && partSrc instanceof SlidePart) {
+            SlidePart slideSrc = (SlidePart) partSrc;
 
-            if (partSrc != null && partSrc instanceof SlidePart) {
-                SlidePart slideSrc = (SlidePart) partSrc;
+            Optional<String> optFirstString = extractFirstString(slideSrc);
 
-                Optional<String> optFirstString = extractFirstString(slideSrc);
-
-                if(!optFirstString.isPresent()) {
-                    log.warn("Slide {} has no first string in the layout. This slide has been ignored.", tgtPartName);
-                }
-                else  {
-                    final String firstString = optFirstString.get();
-
-
-                    log.debug("First String of {} is {}", tgtPartName, firstString);
-
-                    List<String> notesParagraphs = extractParagraphsOfComments(slideSrc);
-
-                    log.debug("List of notes paragraph");
-                    for(String s : notesParagraphs) {
-                        log.debug(s);
-                    }
-
-                    Slide s = new Slide(tgtPartName.getName(), firstString, notesParagraphs);
-
-                    Slide alreadyExist = slides.putIfAbsent(firstString, s);
-                    if(alreadyExist != null) {
-                        log.warn("The slide {} is already associated with the key {} ", alreadyExist.getPartName(), firstString);
-                    }
-
-                }
+            if(!optFirstString.isPresent()) {
+                log.warn("Slide {} has no first string in the layout. This slide has been ignored.", tgtPartName);
             }
+            else  {
+                final String firstString = optFirstString.get();
 
+
+                log.debug("First String of {} is {}", tgtPartName, firstString);
+
+                List<String> notesParagraphs = extractParagraphsOfComments(slideSrc);
+
+                log.debug("List of notes paragraph");
+                for(String s : notesParagraphs) {
+                    log.debug(s);
+                }
+
+                Slide s = new Slide(tgtPartName.getName(), firstString, notesParagraphs);
+
+                Slide alreadyExist = slides.putIfAbsent(firstString, s);
+                if(alreadyExist != null) {
+                    log.warn("The slide {} is already associated with the key {} ", alreadyExist.getPartName(), firstString);
+                }
+
+            }
         }
     }
 
-    private List<String> extractParagraphsOfComments(SlidePart partSrc) throws Docx4JException {
+    List<String> extractParagraphsOfComments(SlidePart partSrc) throws Docx4JException {
 
         Notes notesSrc = partSrc.getNotesSlidePart().getContents();
         GroupShape shapeNotesSrc = notesSrc.getCSld().getSpTree();
         //Append each paragraph content in the list of paragraphs
-        Appender<String, List<String>> paragraphAppender = new Appender<String, List<String>>() {
-            List<String> appender = new ArrayList<>();
-            @Override
-            public List<String> getContent() {
-                return appender;
-            }
-
-            @Override
-            public void accept(String s) {
-                appender.add(s);
-            }
-        };
+        Appender<String, List<String>> paragraphAppender = new ParagraphAppender();
 
         doOnEachParagraph(shapeNotesSrc, paragraphAppender);
         return paragraphAppender.getContent();
     }
 
-    private <T> void doOnEachParagraph(GroupShape shapeNotesSrc, Appender<String, T> paragraphAppender) {
+    <T> void doOnEachParagraph(GroupShape shapeNotesSrc, Appender<String, T> paragraphAppender) {
         for (Object o : shapeNotesSrc.getSpOrGrpSpOrGraphicFrame()) {
             if (o != null) {
                 if (o instanceof Shape) {
@@ -122,27 +107,43 @@ class SlideExtractor {
         }
     }
 
-    private Optional<String> extractFirstString(SlidePart slide) throws CopyCommentException {
+    Optional<String> extractFirstString(SlidePart slide) throws CopyCommentException {
         if(slide == null || slide.getResolvedLayout() == null || slide.getResolvedLayout().getShapeTree() == null) {
             throw new CopyCommentException("A part of the shape tree of the slide is null");
         }
 
-        Appender<String, Optional<String>> firstOneExtractor = new Appender<String, Optional<String>>() {
-            String firstString;
-
-            @Override
-            public Optional<String> getContent() {
-                return firstString == null ? Optional.empty() : Optional.of(firstString) ;
-            }
-
-            @Override
-            public void accept(String s) {
-                if(firstString == null) firstString = s;
-            }
-        };
+        Appender<String, Optional<String>> firstOneExtractor = new FirstStringAppender();
 
         doOnEachParagraph(slide.getResolvedLayout().getShapeTree() , firstOneExtractor);
 
         return firstOneExtractor.getContent();
+    }
+
+    static class FirstStringAppender implements Appender<String, Optional<String>> {
+        String firstString;
+
+        @Override
+        public Optional<String> getContent() {
+            return firstString == null ? Optional.empty() : Optional.of(firstString) ;
+        }
+
+        @Override
+        public void accept(String s) {
+            if(firstString == null) firstString = s;
+        }
+    }
+
+    static class ParagraphAppender implements Appender<String, List<String>> {
+        List<String> appender = new ArrayList<>();
+
+        @Override
+        public List<String> getContent() {
+            return appender;
+        }
+
+        @Override
+        public void accept(String s) {
+            appender.add(s);
+        }
     }
 }
